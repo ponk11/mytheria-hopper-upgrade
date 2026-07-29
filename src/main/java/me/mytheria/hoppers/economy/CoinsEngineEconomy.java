62eecf4865ace7e432ac925a05d3dbb9978d38a5
ponk <159983729+ponk11@users.noreply.github.com>
2026-07-30 01:52:36 +0200

    Create CoinsEngineEconomy.java

---
+++ src/main/java/me/mytheria/hoppers/economy/CoinsEngineEconomy.java
@@ -0,0 +1,28 @@
+package me.mytheria.hoppers.economy;
+
+import org.bukkit.entity.Player;
+
+public class CoinsEngineEconomy implements EconomyProvider {
+
+
+    @Override
+    public boolean has(Player player, double amount) {
+
+        return false;
+    }
+
+
+    @Override
+    public boolean withdraw(Player player, double amount) {
+
+        return false;
+    }
+
+
+    @Override
+    public String getName() {
+
+        return "CoinsEngine";
+
+    }
+}
