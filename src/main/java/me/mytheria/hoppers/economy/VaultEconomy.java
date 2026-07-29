3e39fee04d039183cc01affd3acbb21c05d821e9
ponk <159983729+ponk11@users.noreply.github.com>
2026-07-30 01:50:44 +0200

    Implement VaultEconomy class for economy management

---
+++ src/main/java/me/mytheria/hoppers/economy/VaultEconomy.java
@@ -0,0 +1,45 @@
+package me.mytheria.hoppers.economy;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.entity.Player;
+
+public class VaultEconomy implements EconomyProvider {
+
+
+    private final Economy economy;
+
+
+    public VaultEconomy(Economy economy) {
+        this.economy = economy;
+    }
+
+
+    @Override
+    public boolean has(Player player, double amount) {
+
+        return economy.has(
+                player,
+                amount
+        );
+
+    }
+
+
+    @Override
+    public boolean withdraw(Player player, double amount) {
+
+        return economy.withdrawPlayer(
+                player,
+                amount
+        ).transactionSuccess();
+
+    }
+
+
+    @Override
+    public String getName() {
+
+        return "Vault";
+
+    }
+}
