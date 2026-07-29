6ad08f9f1ed9127de8ba5df66ec3afdc748c8485
ponk <159983729+ponk11@users.noreply.github.com>
2026-07-30 01:50:20 +0200

    Create EconomyProvider.java

---
+++ src/main/java/me/mytheria/hoppers/economy/EconomyProvider.java
@@ -0,0 +1,16 @@
+package me.mytheria.hoppers.economy;
+
+import org.bukkit.entity.Player;
+
+public interface EconomyProvider {
+
+
+    boolean has(Player player, double amount);
+
+
+    boolean withdraw(Player player, double amount);
+
+
+    String getName();
+
+}
