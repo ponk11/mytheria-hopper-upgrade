534b295be296bae449587e2f938a69e93283f97f
ponk <159983729+ponk11@users.noreply.github.com>
2026-07-30 01:58:01 +0200

    Update GUIListener.java

--- src/main/java/me/mytheria/hoppers/listeners/GUIListener.java
+++ src/main/java/me/mytheria/hoppers/listeners/GUIListener.java
@@ -1,9 +1,12 @@
 package me.mytheria.hoppers.listeners;
 
 import me.mytheria.hoppers.MytheriaHoppers;
+import me.mytheria.hoppers.hopper.HopperData;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.entity.Player;
 
 public class GUIListener implements Listener {
 
@@ -16,18 +19,114 @@ public class GUIListener implements Listener {
     }
 
 
+
     @EventHandler
-    public void onClick(InventoryClickEvent event) {
+    public void click(InventoryClickEvent event) {
 
 
-        if (event.getView()
+        if (!event.getView()
                 .getTitle()
-                .contains("Mytheria Hoppers")) {
+                .contains("Mytheria")) {
+
+            return;
+        }
 
 
-            event.setCancelled(true);
+        event.setCancelled(true);
+
+
+        if (!(event.getWhoClicked() instanceof Player player)) {
+            return;
+        }
+
+
+
+        HopperData data =
+                plugin.getHopperManager()
+                        .getData(
+                                player.getLocation()
+                        );
+
+
+
+        if (event.getSlot() == 11) {
+
+
+            int level =
+                    data.getSpeedLevel() + 1;
+
+
+            if (level >
+                    plugin.getConfig()
+                            .getInt(
+                                    "settings.max-speed-level"
+                            )) {
+
+
+                player.sendMessage(
+                        color("&cMaximum level reached.")
+                );
+
+                return;
+
+            }
+
+
+            data.setSpeedLevel(level);
+
+
+            player.sendMessage(
+                    color("&dMytheria Hoppers &8» &aSpeed upgraded!")
+            );
+
+
+        }
+
+
+
+        if (event.getSlot() == 15) {
+
+
+            int level =
+                    data.getRangeLevel() + 1;
+
+
+            if (level >
+                    plugin.getConfig()
+                            .getInt(
+                                    "settings.max-range-level"
+                            )) {
+
+
+                player.sendMessage(
+                        color("&cMaximum level reached.")
+                );
+
+                return;
+
+            }
+
+
+            data.setRangeLevel(level);
+
+
+            player.sendMessage(
+                    color("&dMytheria Hoppers &8» &aRange upgraded!")
+            );
 
         }
 
     }
+
+
+
+    private String color(String text) {
+
+        return ChatColor.translateAlternateColorCodes(
+                '&',
+                text
+        );
+
+    }
+
 }
