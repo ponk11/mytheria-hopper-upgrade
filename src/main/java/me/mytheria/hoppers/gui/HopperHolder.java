40157a0fce0921ed310dcff9c85c2045b7d06ecc
ponk <159983729+ponk11@users.noreply.github.com>
2026-07-30 02:14:31 +0200

    Update HopperHolder.java

--- src/main/java/me/mytheria/hoppers/gui/HopperHolder.java
+++ src/main/java/me/mytheria/hoppers/gui/HopperHolder.java
@@ -1,5 +1,6 @@
 package me.mytheria.hoppers.gui;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
@@ -9,19 +10,47 @@ public class HopperHolder implements InventoryHolder {
 
     private final Location location;
 
+    private Inventory inventory;
+
 
     public HopperHolder(Location location) {
+
         this.location = location;
+
     }
+
 
 
     public Location getLocation() {
+
         return location;
+
     }
+
+
+
+    public void setInventory(Inventory inventory) {
+
+        this.inventory = inventory;
+
+    }
+
 
 
     @Override
     public Inventory getInventory() {
-        return null;
+
+        if (inventory == null) {
+
+            inventory = Bukkit.createInventory(
+                    this,
+                    27
+            );
+
+        }
+
+        return inventory;
+
     }
+
 }
