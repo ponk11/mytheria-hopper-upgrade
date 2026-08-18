f1f3f6584b06b50df007cb2083ea5d6de6335b7b
ponk <159983729+ponk11@users.noreply.github.com>
2026-08-18 14:21:44 -0400

    Update HopperBreakListener.java

--- src/main/java/me/mytheria/hoppers/listeners/HopperBreakListener.java
+++ src/main/java/me/mytheria/hoppers/listeners/HopperBreakListener.java
@@ -1,10 +1,18 @@
 package me.mytheria.hoppers.listeners;
 
 import me.mytheria.hoppers.MytheriaHoppers;
+import me.mytheria.hoppers.hopper.HopperData;
+import me.mytheria.hoppers.hopper.HopperKeys;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 public class HopperBreakListener implements Listener {
 
@@ -17,15 +25,68 @@ public class HopperBreakListener implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
 
-        if (event.getBlock().getType() != Material.HOPPER) {
+        Block block = event.getBlock();
+
+        if (block.getType() != Material.HOPPER) {
             return;
         }
 
+        HopperData data =
+                plugin.getHopperManager()
+                        .getData(block.getLocation());
+
+        if (data == null) {
+            return;
+        }
+
+        int speedLevel =
+                data.getSpeedLevel();
+
+        int rangeLevel =
+                data.getRangeLevel();
+
         plugin.getHopperManager()
-                .remove(
-                        event.getBlock().getLocation()
-                );
+                .remove(block.getLocation());
 
         plugin.getDataManager().save();
+
+        if (speedLevel <= 0 && rangeLevel <= 0) {
+            return;
+        }
+
+        event.setDropItems(false);
+
+        ItemStack hopper =
+                new ItemStack(Material.HOPPER);
+
+        ItemMeta meta =
+                hopper.getItemMeta();
+
+        if (meta == null) {
+            return;
+        }
+
+        meta.getPersistentDataContainer().set(
+                HopperKeys.speedLevel(),
+                PersistentDataType.INTEGER,
+                speedLevel
+        );
+
+        meta.getPersistentDataContainer().set(
+                HopperKeys.rangeLevel(),
+                PersistentDataType.INTEGER,
+                rangeLevel
+        );
+
+        meta.setDisplayName(
+                "§dMytheria Hopper"
+        );
+
+        hopper.setItemMeta(meta);
+
+        block.getWorld().dropItemNaturally(
+                block.getLocation(),
+                hopper
+        );
     }
 }
