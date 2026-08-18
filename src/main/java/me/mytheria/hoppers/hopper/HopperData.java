b9cc4d0bf3a711fa041823204dabfad549c5b328
ponk <159983729+ponk11@users.noreply.github.com>
2026-08-18 14:10:24 -0400

    made it linked to the other thing

--- src/main/java/me/mytheria/hoppers/hopper/HopperData.java
+++ src/main/java/me/mytheria/hoppers/hopper/HopperData.java
@@ -5,32 +5,35 @@ public class HopperData {
     private int speedLevel;
     private int rangeLevel;
 
+    private long lastTransferTick;
 
     public HopperData() {
-
-        speedLevel = 0;
-        rangeLevel = 0;
-
+        this.speedLevel = 0;
+        this.rangeLevel = 0;
+        this.lastTransferTick = 0;
     }
-
 
     public int getSpeedLevel() {
         return speedLevel;
     }
 
-
     public void setSpeedLevel(int speedLevel) {
         this.speedLevel = speedLevel;
     }
-
 
     public int getRangeLevel() {
         return rangeLevel;
     }
 
-
     public void setRangeLevel(int rangeLevel) {
         this.rangeLevel = rangeLevel;
     }
 
+    public long getLastTransferTick() {
+        return lastTransferTick;
+    }
+
+    public void setLastTransferTick(long lastTransferTick) {
+        this.lastTransferTick = lastTransferTick;
+    }
 }
