60ae156497d8660d6b0bd9337ef3edadf0890240
ponk <159983729+ponk11@users.noreply.github.com>
2026-08-18 14:08:19 -0400

    made this uselkess for now
    Removed unused currency field and related logic.

--- src/main/java/me/mytheria/hoppers/economy/CoinsEngineEconomy.java
+++ src/main/java/me/mytheria/hoppers/economy/CoinsEngineEconomy.java
@@ -1,116 +1,21 @@
 package me.mytheria.hoppers.economy;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
-import java.lang.reflect.Method;
 
 public class CoinsEngineEconomy implements EconomyProvider {
 
-
-    private final String currency;
-
-
-    public CoinsEngineEconomy(String currency) {
-
-        this.currency = currency;
-
-    }
-
-
-
     @Override
     public boolean has(Player player, double amount) {
-
-        try {
-
-            Class<?> api =
-                    Class.forName(
-                            "su.nightexpress.coinsengine.api.CoinsEngineAPI"
-                    );
-
-
-            Method method =
-                    api.getMethod(
-                            "getBalance",
-                            Player.class,
-                            String.class
-                    );
-
-
-            Object result =
-                    method.invoke(
-                            null,
-                            player,
-                            currency
-                    );
-
-
-            if (result instanceof Number number) {
-
-                return number.doubleValue() >= amount;
-
-            }
-
-
-        } catch (Exception ignored) {
-
-        }
-
-
         return false;
-
     }
-
-
 
     @Override
     public boolean withdraw(Player player, double amount) {
-
-        try {
-
-            Class<?> api =
-                    Class.forName(
-                            "su.nightexpress.coinsengine.api.CoinsEngineAPI"
-                    );
-
-
-            Method method =
-                    api.getMethod(
-                            "removeBalance",
-                            Player.class,
-                            String.class,
-                            double.class
-                    );
-
-
-            method.invoke(
-                    null,
-                    player,
-                    currency,
-                    amount
-            );
-
-
-            return true;
-
-
-        } catch (Exception ignored) {
-
-        }
-
-
         return false;
-
     }
-
-
 
     @Override
     public String getName() {
-
         return "CoinsEngine";
-
     }
-
 }
