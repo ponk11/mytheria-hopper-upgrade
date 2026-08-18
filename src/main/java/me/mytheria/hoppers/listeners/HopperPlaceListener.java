16c0bd65d2cb24e81fa12c4e373bef3410653cfb
ponk <159983729+ponk11@users.noreply.github.com>
2026-08-18 14:22:00 -0400

    Update HopperPlaceListener.java

--- src/main/java/me/mytheria/hoppers/listeners/HopperPlaceListener.java
+++ src/main/java/me/mytheria/hoppers/listeners/HopperPlaceListener.java
@@ -2,10 +2,14 @@ package me.mytheria.hoppers.listeners;
 
 import me.mytheria.hoppers.MytheriaHoppers;
 import me.mytheria.hoppers.hopper.HopperData;
+import me.mytheria.hoppers.hopper.HopperKeys;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
 
 public class HopperPlaceListener implements Listener {
 
@@ -22,11 +26,59 @@ public class HopperPlaceListener implements Listener {
             return;
         }
 
+        ItemStack item =
+                event.getItemInHand();
+
+        int speedLevel = 0;
+        int rangeLevel = 0;
+
+        if (item.hasItemMeta()) {
+
+            if (item.getItemMeta()
+                    .getPersistentDataContainer()
+                    .has(
+                            HopperKeys.speedLevel(),
+                            PersistentDataType.INTEGER
+                    )) {
+
+                speedLevel =
+                        item.getItemMeta()
+                                .getPersistentDataContainer()
+                                .get(
+                                        HopperKeys.speedLevel(),
+                                        PersistentDataType.INTEGER
+                                );
+            }
+
+            if (item.getItemMeta()
+                    .getPersistentDataContainer()
+                    .has(
+                            HopperKeys.rangeLevel(),
+                            PersistentDataType.INTEGER
+                    )) {
+
+                rangeLevel =
+                        item.getItemMeta()
+                                .getPersistentDataContainer()
+                                .get(
+                                        HopperKeys.rangeLevel(),
+                                        PersistentDataType.INTEGER
+                                );
+            }
+        }
+
+        HopperData data =
+                new HopperData();
+
+        data.setSpeedLevel(speedLevel);
+        data.setRangeLevel(rangeLevel);
+
         plugin.getHopperManager()
                 .getHoppers()
                 .put(
-                        event.getBlock().getLocation(),
-                        new HopperData()
+                        event.getBlock()
+                                .getLocation(),
+                        data
                 );
 
         plugin.getDataManager().save();
