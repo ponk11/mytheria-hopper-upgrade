cd1f380b1b62958686203f056b7796cec9bc63b7
ponk <159983729+ponk11@users.noreply.github.com>
2026-08-18 14:11:02 -0400

    made the prices match prices

--- src/main/java/me/mytheria/hoppers/gui/UpgradeManager.java
+++ src/main/java/me/mytheria/hoppers/gui/UpgradeManager.java
@@ -1,127 +1,119 @@
 package me.mytheria.hoppers.gui;
 
 import me.mytheria.hoppers.MytheriaHoppers;
-import me.mytheria.hoppers.hopper.HopperData;
 import me.mytheria.hoppers.economy.EconomyProvider;
+import me.mytheria.hoppers.hopper.HopperData;
 import org.bukkit.entity.Player;
 
 public class UpgradeManager {
 
-
     private final MytheriaHoppers plugin;
-
 
     public UpgradeManager(MytheriaHoppers plugin) {
         this.plugin = plugin;
     }
 
+    public boolean upgradeSpeed(
+            Player player,
+            HopperData data
+    ) {
 
-
-    public boolean upgradeSpeed(Player player, HopperData data) {
-
-
-        int next =
+        int nextLevel =
                 data.getSpeedLevel() + 1;
 
-
-        int cost =
-                plugin.getConfig()
-                        .getInt(
-                        "speed-upgrades."
-                        + next
-                        + ".cost"
+        int maxLevel =
+                plugin.getConfig().getInt(
+                        "settings.max-speed-level",
+                        10
                 );
 
+        if (nextLevel > maxLevel) {
+            return false;
+        }
+
+        String path =
+                "speed-upgrades."
+                        + nextLevel;
+
+        if (!plugin.getConfig().contains(path)) {
+            return false;
+        }
+
+        double cost =
+                plugin.getConfig().getDouble(
+                        path + ".cost"
+                );
 
         EconomyProvider economy =
                 plugin.getEconomyManager()
                         .getProvider();
 
-
-
         if (economy == null) {
-
             return false;
-
         }
-
-
 
         if (!economy.has(player, cost)) {
-
             return false;
-
         }
 
+        if (!economy.withdraw(player, cost)) {
+            return false;
+        }
 
-
-        economy.withdraw(
-                player,
-                cost
-        );
-
-
-        data.setSpeedLevel(next);
-
+        data.setSpeedLevel(nextLevel);
 
         return true;
-
     }
 
+    public boolean upgradeRange(
+            Player player,
+            HopperData data
+    ) {
 
-
-
-
-    public boolean upgradeRange(Player player, HopperData data) {
-
-
-        int next =
+        int nextLevel =
                 data.getRangeLevel() + 1;
 
-
-        int cost =
-                plugin.getConfig()
-                        .getInt(
-                        "range-upgrades."
-                        + next
-                        + ".cost"
+        int maxLevel =
+                plugin.getConfig().getInt(
+                        "settings.max-range-level",
+                        10
                 );
 
+        if (nextLevel > maxLevel) {
+            return false;
+        }
 
+        String path =
+                "range-upgrades."
+                        + nextLevel;
+
+        if (!plugin.getConfig().contains(path)) {
+            return false;
+        }
+
+        double cost =
+                plugin.getConfig().getDouble(
+                        path + ".cost"
+                );
 
         EconomyProvider economy =
                 plugin.getEconomyManager()
                         .getProvider();
 
-
-
         if (economy == null) {
-
             return false;
-
         }
-
-
 
         if (!economy.has(player, cost)) {
-
             return false;
-
         }
 
+        if (!economy.withdraw(player, cost)) {
+            return false;
+        }
 
-
-        economy.withdraw(
-                player,
-                cost
-        );
-
-
-        data.setRangeLevel(next);
-
+        data.setRangeLevel(nextLevel);
 
         return true;
-
     }
-
 }
